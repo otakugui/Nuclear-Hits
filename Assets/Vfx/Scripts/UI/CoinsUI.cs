f1624c687bf44c38953c6e32fb125bebb1775216
agehm <guto.gehm@gmail.com>
2020-10-10 23:22:26 -0300

    OrbsTower

---
+++ Assets/Vfx/Scripts/UI/CoinsUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinsUI : MonoBehaviour
+{
+    private int amountOfCoins = 0;
+    [SerializeField] Text MyCoinsText;
+    
+
+
+
+    public void MoreCoins()
+    {
+        amountOfCoins++;
+
+
+        MyCoinsText.text = amountOfCoins.ToString();
+
+    }
+  
+}

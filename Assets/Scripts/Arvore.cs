6271561a2dc9abf21eb425e1c4385a25f1b47f4f
Gabriel Rodrigues <gabriel.rodl@outlook.com>
2020-10-11 14:04:03 -0300

    Scripts da Gameplay
    Para ver a cena funcionando acessar Scene e cena_main
    Para ver os scripts Assets\Scripts

---
+++ Assets/Scripts/Arvore.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arvore : MonoBehaviour
+{
+    public int Vida;
+    public int Dano;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Inimigo"))
+        {
+            //perde vida
+            TomarDano();
+        }
+        
+
+    }
+    public void TomarDano()
+    {
+        Vida = Vida - Dano;
+        if (Vida >= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    //caso o dano nao funcione, destroi direto
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{if (collision.CompareTag("Inimigo"))
+}

cf3e90707fa8f52eb8444312022942b6621796b2
Guilherme L. De Cicco <guilherme.cicco@users.noreply.github.com>
2020-10-11 18:14:47 -0300

    commit
    menu

---
+++ Assets/Scene/MenuPrincipal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public string Cena_main;
+    public string Creditos;
+    public string Retornar;
+
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(Cena_main);
+    }
+
+    public void game()
+    {
+        SceneManager.LoadScene(Creditos);
+    }
+
+    public void voltar()
+    {
+        SceneManager.LoadScene(Retornar);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}

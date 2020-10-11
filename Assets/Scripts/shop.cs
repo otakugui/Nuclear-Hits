f935c24f208de92279734f4093b1280925dedbe0
Gabriel Rodrigues <gabriel.rodl@outlook.com>
2020-10-11 18:22:03 -0300

    correção

---
+++ Assets/Scripts/shop.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shop : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (transform.parent.position - transform.position )* 1* Time.deltaTime;
+    }
+}

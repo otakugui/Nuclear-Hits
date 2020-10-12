43000cc6cff9909fa70ddfc1c029689d04c87c31
Gabriel Rodrigues <gabriel.rodl@outlook.com>
2020-10-11 21:15:11 -0300

    cena_final
    Lenhador não bate na arvore

--- Assets/Scripts/Arvore.cs
+++ Assets/Scripts/Arvore.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 public class Arvore : MonoBehaviour
 {
     public int Vida;
-    public int Dano;
 
     private Animator MyAnim;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -21,19 +20,21 @@ public class Arvore : MonoBehaviour
 
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+
+ 
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.CompareTag("Inimigo"))
+        if (collision.gameObject.tag =="EnemyLenhador")
         {
             //perde vida
             TomarDano();
-        }
-        
 
+        }
     }
+
     public void TomarDano()
     {
-        Vida = Vida - Dano;
+        Vida--;
         if (Vida <= 0)
         {
             MyAnim.SetBool("Died", true);
